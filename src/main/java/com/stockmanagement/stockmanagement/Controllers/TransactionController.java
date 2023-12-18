65d9bb11ce1f851f7fcfa4ff717bf80cda185580
alenjohn05 <alenjohnmj@gmail.com>
2023-12-18 10:00:00 +0530

    updated the users content

---
+++ src/main/java/com/stockmanagement/stockmanagement/Controllers/TransactionController.java
@@ -0,0 +1,47 @@
+package com.stockmanagement.stockmanagement.Controllers;
+
+import com.stockmanagement.stockmanagement.DtoClasses.TransactionDTO;
+import com.stockmanagement.stockmanagement.ServiceClasses.TransactionService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/transaction/")
+@AllArgsConstructor
+public class TransactionController {
+    private TransactionService transactionService;
+
+    @PostMapping()
+    public ResponseEntity<TransactionDTO> addTransaction(@RequestBody TransactionDTO transactionDTO) {
+        try {
+            if (transactionDTO.getT_id() == null || transactionDTO.getT_id().toString().isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        } catch (NullPointerException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        TransactionDTO savedTransaction = transactionService.addTransaction(transactionDTO);
+        return new ResponseEntity<>(savedTransaction, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{t_id}")
+    public ResponseEntity<TransactionDTO> getTransaction(@PathVariable Integer t_id) {
+        TransactionDTO transactionDTO = transactionService.getTransaction(t_id);
+        return new ResponseEntity<>(transactionDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("{t_id}")
+    public ResponseEntity<TransactionDTO> updateTransaction(@PathVariable Integer t_id, @RequestBody TransactionDTO transactionDTO) {
+        TransactionDTO updatedTransaction = transactionService.updateTransaction(t_id, transactionDTO);
+        return new ResponseEntity<>(updatedTransaction, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{t_id}")
+    public ResponseEntity<String> deleteTransaction(@PathVariable Integer t_id) {
+        String msg = transactionService.deleteTransaction(t_id);
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+
+}
